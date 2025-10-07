1c36bc22fcb854701b7e89988e5156596f9f338e
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-10-07 08:09:36 +0200

    update layout

--- nextjs/defaults/src/app/layout.tsx
+++ nextjs/defaults/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/footer/footer";
+import NavLinks from "./components/navbar/navbar";
 
 export const metadata: Metadata = {
   title: "Xdefault",
@@ -18,6 +19,7 @@ export default function RootLayout({
         className={``}
       >
         <header></header>
+        <NavLinks />
         <main>
         {children}
         </main>
